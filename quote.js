de08b33bb2f2263a7aac61286846bbfd16dce49a
manahil jutt <manahilshafaqatalijutt@gmail.com>
2024-05-11 07:34:13 -0700

    Add files via upload

---
+++ quote.js
@@ -0,0 +1,5 @@
+"use strict";
+let quote = "A PERSON WHO NEVER MADE A MISTAKE NEVER TRIED ANYTHING NEW";
+let person_name = "albert Einstien";
+let message = `${person_name} once said ${quote}`;
+console.log(message);
---
+++ quote_author.ts
@@ -0,0 +1,4 @@
+let quote: string= "A PERSON WHO NEVER MADE A MISTAKE NEVER TRIED ANYTHING NEW"
+let person_name: string = "albert Einstien"
+let message = `${person_name} once said ${quote}`
+console.log(message);
